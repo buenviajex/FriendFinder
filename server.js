523210a23a3a402e4bde56633f327cd066ebc9f1
Mark Buenviaje <mbuenviaje@outlook.com>
2018-09-01 23:39:26 -0700

    startup files

---
+++ server.js
@@ -0,0 +1,20 @@
+//Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//Express Configuration
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Router
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+//Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
