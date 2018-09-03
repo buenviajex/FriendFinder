90e645353f7bd49c5a3e0b41a1e3559902327177
Mark Buenviaje <mbuenviaje@outlook.com>
2018-09-03 16:24:40 -0700

    updated with css and api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,13 @@
-app.get("/api/friends", function(req, res) {
-    res.json(friendsTable);
-});
+var friendsArray = require("../data/friends");
 
-app.post("/api/friends", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsArray);
+        console.log(friendsArray);
+    });
 
-});
+    app.post("/api/friends", function(req, res) {
+        friendsArray.push(req.body);
+        res.json(friendsArray)
+    });
+}
