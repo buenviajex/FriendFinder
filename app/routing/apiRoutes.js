8bcce27382f48030cd49543da4789253cbeb1189
Mark Buenviaje <mbuenviaje@outlook.com>
2018-09-03 20:25:16 -0700

    updated api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,28 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        friendsArray.push(req.body);
-        res.json(friendsArray)
+        var newFriend = {
+            name: re.body.name,
+            photo: req.body.photo,
+            scores: JSON.parse(re.body.scores)
+        }
+
+        var friendScore = [];
+
+        friendsArray.forEach(function(item, index) {
+            var difference = 0;
+            for (var i = 0; i < item.scores.length; i++) {
+                difference += Math.abs(item.scores[i] - newFriend.scores[i]);
+            }
+            friendScore.push({ "difference": difference, "index": index });
+        });
+
+        friendScore.sort(function(a, b) {
+            return a.difference - b.difference;
+        });
+
+        friendsArray.push(newFriend);
+        res.json(friends[friendScore[0].index]);
+
     });
 }
